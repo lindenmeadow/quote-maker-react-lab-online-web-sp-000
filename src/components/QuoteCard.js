56a2020784ba0590c8147e52d519f1f1171898d0
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-27 19:46:41 +0000

    Automatically backed up by Learn

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -23,7 +23,7 @@ const QuoteCard = ({ quote, removeQuote, upvoteQuote, downvoteQuote }) => (
           <button
             type="button"
             className="btn btn-secondary"
-            oonClick={() => downvoteQuote(quote.id)}
+            onClick={() => downvoteQuote(quote.id)}
           >
             Downvote
           </button>
