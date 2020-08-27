87235d65395d507a10dfe4846147f480d1378899
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-27 19:39:02 +0000

    Automatically backed up by Learn

--- test/actions/QuotesActionsTest.js
+++ test/actions/QuotesActionsTest.js
@@ -1,10 +1,10 @@
 import { expect } from 'chai';
 import uuid from 'uuid';
 import {
-  addQuote,	
-  removeQuote,	
-  upvoteQuote,	
-  downvoteQuote	
+  addQuote,
+  removeQuote,
+  upvoteQuote,
+  downvoteQuote
 } from '../../src/actions/quotes';
 
 
